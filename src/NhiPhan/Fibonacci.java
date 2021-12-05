f807059f42a1a637996182e279afee6ea5877b20
DungCR7 <buivandung279@gmail.com>
2021-12-05 10:16:17 +0700

    Init project

--- src/NhiPhan/Fibonacci.java
+++ src/NhiPhan/Fibonacci.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = 0;
+        /*int a = 0;
         int soThuNhat = 0,soThuHai=1;
         int soThuBa;
         do {
@@ -17,6 +17,17 @@ public class Fibonacci {
             soThuNhat=soThuHai;
             soThuHai=soThuBa;
             System.out.print("\t "+soThuBa);
+        }
+         */
+        System.out.print("Chiều dài fibonacci:");
+        int n = scanner.nextInt();
+        int t = 1;
+        int s = 0;
+        for (int i=1; i <=n; i++ ) {
+            int a = s;
+            s = s + t;
+            t = a;
+            System.out.print(s + ", ");
         }
     }
 }
--- src/NhiPhan/TinhTongTrungBinh.java
+++ src/NhiPhan/TinhTongTrungBinh.java
@@ -18,25 +18,23 @@ public class TinhTongTrungBinh {
         /*int i=1;
         while (i<=100) {
             System.out.println(i);
-            i++;
           tong+=i;
-          trungBinhCong=(float) tong/100;
+          i++;
         }
-        tong+=i;
         trungBinhCong=(float) tong/100;
         System.out.println("Tong cac so vua nhap la :" + tong);
         System.out.println(new StringBuilder().append("Trung binh cong cac so vua nhap la :").append(trungBinhCong).toString());
 
          */
-
         int i = 0;
         do {
             tong+=i;
             i++;
         }while (i<=100);
-        System.out.println(i);
         System.out.println(new StringBuilder().append("Tong tu 1 den 100 la ").append(tong).toString());
         trungBinhCong=(float) tong/100;
         System.out.println(new StringBuilder().append("Trung binh cong cac so tu 1 den 100 la :").append(trungBinhCong).toString());
+
+
     }
 }
